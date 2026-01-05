f9f9cf3163536777ffa322bde69b76052fa91213
AshishTripathi80 <tripashi6@gmail.com>
2026-01-05 19:00:13 +0530

    initial commit

---
+++ src/main/java/com/aiassistant/embedding/EmbeddingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aiassistant.embedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddingServiceApplication.class, args);
+	}
+
+}
